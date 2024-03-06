b0b5433ef9f669fc03dfe1e6e0a8b392ac45e3bf
Igor Oliveira <igoroliveira9827@gmail.com>
2024-03-06 20:41:44 -0300

    feat: create a new mechanic entity

---
+++ Autoease.Domain/Entities/Mechanic.cs
@@ -0,0 +1,9 @@
+namespace Autoease.Domain.Entities;
+
+public class Mechanic : BaseEntity 
+{
+    public string Name { get; private set; }
+    public string LastName { get; private set; }
+    public string Register { get; private set; }
+
+}
