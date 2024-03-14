6080fca7c92dd951b3551cff14d57e2e1ebd600b
Igor Oliveira <igoroliveira9827@gmail.com>
2024-03-14 01:05:20 -0300

    feat: create new validation for user

--- Autoease.Domain/Entities/User.cs
+++ Autoease.Domain/Entities/User.cs
@@ -1,11 +1,25 @@
+using Autoease.Domain.ValueObjects;
+
 namespace Autoease.Domain.Entities;
 
 public class User : BaseEntity
 {
+    private readonly List<string> errors = new();
+
     public Guid Id { get; private set; } = new Guid();
     public string Username { get; private set; }
     public string Email { get; private set; }
     public string Password { get; private set; }
     public List<string> Veichles { get; private set; }
+    public Address Address { get; private set; }
 
+    public bool IsValid()
+    {
+        return errors.Count == 0;
+    }
+
+    public void Validate()
+    {
+
+    }
 }
