1991b5760f649c2dd2f6c84f674c5f908a0ea553
Igor Oliveira <igoroliveira9827@gmail.com>
2024-03-20 20:19:57 -0300

    feat: add object value veichle to the appoitment aggregation

--- Autoease.Domain/Aggregation/Appointment.cs
+++ Autoease.Domain/Aggregation/Appointment.cs
@@ -1,14 +1,27 @@
 using Autoease.Domain.Entities;
 using Autoease.Domain.Enum;
+using Autoease.Domain.ValueObjects;
 
 namespace Autoease.Domain.Aggregation;
 
 public class Appointment : BaseEntity
 {
+    public Appointment(Appointment appointment_date, ServiceType serviceType, Veichle veichle, string description, Guid garageId, Garage garage, Guid userId, User user)
+    {
+        Appointment_date = appointment_date;
+        ServiceType = serviceType;
+        Veichle = veichle;
+        Description = description;
+        GarageId = garageId;
+        Garage = garage;
+        UserId = userId;
+        User = user;
+    }
+
     public Appointment Appointment_date { get; private set; }
     public ServiceType ServiceType { get; private set; }
-    public string Veichle { get; private set; }
-    public string Description { get; set; }
+    public Veichle Veichle { get; private set; }
+    public string Description { get; private set; }
     public Guid GarageId { get; private set; }
     public Garage Garage { get; private set; }
     public Guid UserId { get; private set; }
