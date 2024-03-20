4e096b4dbf0cdee814f7a3857524d5c544a914dd
Igor Oliveira <igoroliveira9827@gmail.com>
2024-03-20 18:42:35 -0300

    refactor: add new lines in appointment entity

--- Autoease.Domain/Entities/Appointment.cs
+++ Autoease.Domain/Entities/Appointment.cs
@@ -1,6 +1,15 @@
+using Autoease.Domain.Enum;
+
 namespace Autoease.Domain.Entities;
 
 public class Appointment : BaseEntity
 {
-
+    public Appointment Appointment_date { get; private set; }
+    public ServiceType ServiceType { get; private set; }
+    public string Veichle { get; private set; }
+    public string Description { get; set; }
+    public Guid MechanicId { get; private set; }
+    public Mechanic Mechanic { get; private set; }
+    public Guid UserId { get; private set; }
+    public User User { get; private set; }
 }
