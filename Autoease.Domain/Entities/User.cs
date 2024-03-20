ab439744abcecbfa938de480f9521e2702447309
Igor Oliveira <igoroliveira9827@gmail.com>
2024-03-20 20:19:15 -0300

    refactor: fix user validation issues

--- Autoease.Domain/Entities/User.cs
+++ Autoease.Domain/Entities/User.cs
@@ -6,11 +6,25 @@ public class User : BaseEntity
 {
     private readonly List<string> errors = new();
 
-    public Guid Id { get; private set; } = new Guid();
+    public User(string userCPF, string username, string email, string password, string name, string lastName, Address address)
+    {
+        UserCPF = userCPF;
+        Username = username;
+        Email = email;
+        Password = password;
+        Name = name;
+        LastName = lastName;
+        Address = address;
+
+        Validate();
+    }
+
+    public string UserCPF { get; private set; }
     public string Username { get; private set; }
     public string Email { get; private set; }
     public string Password { get; private set; }
-    public List<string> Veichles { get; private set; }
+    public string Name { get; private set; }
+    public string LastName { get; private set; }
     public Address Address { get; private set; }
 
     public bool IsValid()
@@ -20,6 +34,8 @@ public class User : BaseEntity
 
     public void Validate()
     {
-
+        if(string.IsNullOrEmpty(Username)) errors.Add("Username cannot be empty!");
+        if(string.IsNullOrEmpty(Email)) errors.Add("Email cannot be empty!");
+        if(string.IsNullOrEmpty(Password) && Password.Length < 6) errors.Add("Username cannot be empity!");
     }
 }
