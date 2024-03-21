e79ade84307d036c98d7f278ae9e734eaade5006
Igor Oliveira <igoroliveira9827@gmail.com>
2024-03-21 19:34:23 -0300

    feat: set AddScoped to dependency injection between interface and service

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Autoease.Domain.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Repositories
+
+builder.Services.AddScoped<ICreateUser, CreateUser>();
 
 var app = builder.Build();
 
