ead163ccd5b1a69b423a5b0e4cd59ce420873616
Igor Oliveira <igoroliveira9827@gmail.com>
2024-03-29 16:49:27 -0300

    feat: add properties in db context

--- Autoease.Infrastructure/Persistence/DbContext.cs
+++ Autoease.Infrastructure/Persistence/DbContext.cs
@@ -1,9 +1,25 @@
 
+using Autoease.Domain.Aggregation;
+using Autoease.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Autoease.Infrastructure.Persistence;
 
 public class DatabaseContext : DbContext
 {
-    
+    protected readonly IConfiguration Configuration;
+
+    public DatabaseContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(Configuration.GetConnectionString("DatabaseContext"));
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Garage> Garages { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
 }
