311ebacebd9f758542927a7f74b4133ec4561dcf
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-03 19:28:07 -0300

    feat: create a new base entity for the entities models

---
+++ Autoease.Domain/Entities/BaseEntity .cs
@@ -0,0 +1,13 @@
+namespace Autoease.Domain.Entities;
+
+public class BaseEntity
+{
+    public Guid Id { get; private set; }
+    public DateTime Created_At { get; private set; }
+
+    public BaseEntity ()
+    {
+        Id = new Guid();
+        Created_At = new DateTime();
+    }
+}
