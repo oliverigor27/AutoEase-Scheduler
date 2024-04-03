2d988fc81eface67aa7515f2e6ba59249e86546f
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-03 19:28:44 -0300

    feat: create a new entity address

---
+++ Autoease.Domain/Entities/Address.cs
@@ -0,0 +1,24 @@
+namespace Autoease.Domain.Entities;
+
+public class Address
+{
+    public string Street { get; private set; }
+    public string City { get; private set; }
+    public string State { get; private set; }
+    public string Country { get; private set; }
+    public string PostalCode { get; private set; }
+
+    public Address(string street, string city, string state, string country, string postalCode)
+    {
+        Street = street;
+        City = city;
+        State = state;
+        Country = country;
+        PostalCode = postalCode;
+    }    
+    
+    protected Address()
+    {
+        
+    }
+}
