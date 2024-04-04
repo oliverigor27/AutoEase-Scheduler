a7a64413488e76e64f90bea64f5e1c47037b149a
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-04 18:37:11 -0300

    feat: create a new entity garage

---
+++ Autoease.Domain/Entities/Garage.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using Autoease.Domain.ValueObjects;
+
+namespace Autoease.Domain.Entities;
+
+public class GarageEntity
+{
+    protected GarageEntity() {}
+    public GarageEntity(string garageRegister, string garageName, Address address, string email, string phone, string businessHour, List<string> services)
+    {
+        GarageRegister = garageRegister;
+        GarageName = garageName;
+        Address = address;
+        Email = email;
+        Phone = phone;
+        BusinessHour = businessHour;
+        Services = services;
+    }
+
+    [Key]
+    public string GarageRegister { get; private set; }
+    public string GarageName { get; private set; }
+    public Address Address { get; private set; }
+    public string Email { get; private set; }
+    public string Phone { get; private set; }
+    public string BusinessHour { get; private set; }
+    public List<string> Services { get; private set; }
+}
