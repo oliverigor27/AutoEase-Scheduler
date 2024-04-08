bd402271349f98cef8466381ce7c4c2a2371af8c
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-08 19:13:28 -0300

    refactor: add a new migration that remove wrong fields in database

--- Migrations/20240408210921_removal_fields.Designer.cs
+++ Migrations/20240408210921_removal_fields.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Autoease.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AutoEase_Scheduler.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20240408210921_removal_fields")]
+    partial class removal_fields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
