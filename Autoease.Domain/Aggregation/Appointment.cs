ba12dfc9b5453d90464840796bb865fb84c55a07
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-08 19:15:02 -0300

    refactor: remove wrong fields

--- Autoease.Domain/Aggregation/Appointment.cs
+++ Autoease.Domain/Aggregation/Appointment.cs
@@ -8,22 +8,18 @@ public class Appointment : BaseEntity
     public Appointment()
     {
     }
-    public Appointment(DateTime appointmentDate, string note, Guid garageId, GarageEntity garage, Guid userId, UserEntity user, Veichle veichle)
+    public Appointment(DateTime appointmentDate, string note, GarageEntity garage, UserEntity user, Veichle veichle)
     {
         AppointmentDate = appointmentDate;
         Note = note;
-        GarageId = garageId;
         Garage = garage;
-        UserId = userId;
         User = user;
         Veichle = veichle;
     }
 
     public DateTime AppointmentDate { get; private set; }
     public string Note { get; private set; }
-    public Guid GarageId { get; private set; }
     public GarageEntity Garage { get; private set; }
-    public Guid UserId { get; private set; }
     public UserEntity User { get; private set; }
     public Veichle Veichle { get; private set; }
 }
