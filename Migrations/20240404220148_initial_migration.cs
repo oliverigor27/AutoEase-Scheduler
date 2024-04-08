e0d4c1e4e1f4978f5a56259720c9ff453cffa165
Igor Oliveira <igoroliveira9827@gmail.com>
2024-04-08 17:14:37 -0300

    feat: add the first migration for the database

---
+++ Migrations/20240404220148_initial_migration.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoEase_Scheduler.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Garages",
+                columns: table => new
+                {
+                    GarageRegister = table.Column<string>(type: "text", nullable: false),
+                    GarageName = table.Column<string>(type: "text", nullable: false),
+                    Address_Street = table.Column<string>(type: "text", nullable: false),
+                    Address_City = table.Column<string>(type: "text", nullable: false),
+                    Address_State = table.Column<string>(type: "text", nullable: false),
+                    Address_Country = table.Column<string>(type: "text", nullable: false),
+                    Address_PostalCode = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    BusinessHour = table.Column<string>(type: "text", nullable: false),
+                    Services = table.Column<List<string>>(type: "text[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Garages", x => x.GarageRegister);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserIdCard = table.Column<string>(type: "text", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Address_Street = table.Column<string>(type: "text", nullable: false),
+                    Address_City = table.Column<string>(type: "text", nullable: false),
+                    Address_State = table.Column<string>(type: "text", nullable: false),
+                    Address_Country = table.Column<string>(type: "text", nullable: false),
+                    Address_PostalCode = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserIdCard);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Appointments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AppointmentDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Note = table.Column<string>(type: "text", nullable: false),
+                    GarageId = table.Column<Guid>(type: "uuid", nullable: false),
+                    GarageRegister = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserIdCard = table.Column<string>(type: "text", nullable: false),
+                    Veichle_Brand = table.Column<string>(type: "text", nullable: false),
+                    Veichle_Model = table.Column<string>(type: "text", nullable: false),
+                    Veichle_Year = table.Column<string>(type: "text", nullable: false),
+                    Created_At = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Garages_GarageRegister",
+                        column: x => x.GarageRegister,
+                        principalTable: "Garages",
+                        principalColumn: "GarageRegister",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Appointments_Users_UserIdCard",
+                        column: x => x.UserIdCard,
+                        principalTable: "Users",
+                        principalColumn: "UserIdCard",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_GarageRegister",
+                table: "Appointments",
+                column: "GarageRegister");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointments_UserIdCard",
+                table: "Appointments",
+                column: "UserIdCard");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Garages");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
