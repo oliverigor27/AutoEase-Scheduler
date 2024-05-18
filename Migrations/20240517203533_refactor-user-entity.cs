17edc3687e2a6c96e218f75b4ef5f75a68bf354f
Igor Oliveira <igoroliveira9827@gmail.com>
2024-05-17 23:56:55 -0300

    feat: generate new migration for user table by removing old fields and add role

---
+++ Migrations/20240517203533_refactor-user-entity.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoEase_Scheduler.Migrations
+{
+    /// <inheritdoc />
+    public partial class refactoruserentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address_City",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Address_Country",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Address_PostalCode",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Address_State",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Address_Street",
+                table: "Users",
+                newName: "Role");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Role",
+                table: "Users",
+                newName: "Address_Street");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_City",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Country",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_PostalCode",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_State",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
