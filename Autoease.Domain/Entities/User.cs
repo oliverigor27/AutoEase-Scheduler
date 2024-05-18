64ea3f550f17175e8bfd26e74c46aa5d3d15904e
Igor Oliveira <igoroliveira9827@gmail.com>
2024-05-17 23:53:58 -0300

    feat: add new email and password validation in the user entity

--- Autoease.Domain/Entities/User.cs
+++ Autoease.Domain/Entities/User.cs
@@ -1,45 +1,73 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Autoease.Domain.ValueObjects;
+using System.Text.RegularExpressions;
 
-namespace Autoease.Domain.Entities;
-
-public class UserEntity
+namespace Autoease.Domain.Entities
 {
-    private readonly List<string> errors = new();
-
-    [Key]
-    public string UserIdCard { get; private set; }
-    public string Username { get; private set; }
-    public string Email { get; private set; }
-    public string Password { get; private set; }
-    public string FirstName { get; private set; }
-    public string LastName { get; private set; }
-    public Address Address { get; private set; }
-
-    
-    protected UserEntity() {}
-    public UserEntity(string userIdCard, string username, string email, string password, string firstName, string lastName, Address address)
+    public class UserEntity
     {
-        UserIdCard = userIdCard;
-        Username = username;
-        Email = email;
-        Password = password;
-        FirstName = firstName;
-        LastName = lastName;
-        Address = address;
+        private readonly List<string> errors = new List<string>();
 
-        Validate();
-    }
+        [Key]
+        public string UserIdCard { get; private set; }
+        public string Username { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Role { get; private set; }
 
-    public bool IsValid()
-    {
-        return errors.Count == 0;
-    }
+        // Construtor privado para evitar criação de instâncias sem parâmetros
+        private UserEntity() { }
 
-    private void Validate()
-    {
-        if(UserIdCard == null && UserIdCard.Length < 8)  errors.Add("User Id Card cannot be empty and must have 8 or more characters");
-        if(Username.Length < 2 && string.IsNullOrEmpty(Username)) errors.Add("Username cannot be empty and must have more than 3 caractres");
+        public UserEntity(string userIdCard, string username, string email, string password, string firstName, string lastName, string role)
+        {
+            UserIdCard = userIdCard;
+            Username = username;
+            Email = email;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            Role = role;
+
+            Validate();
+        }
+
+        public bool IsValid()
+        {
+            return errors.Count == 0;
+        }
+
+        private void Validate()
+        {
+            if (string.IsNullOrEmpty(UserIdCard) || UserIdCard.Length < 11)
+                errors.Add("User Id Card cannot be empty and must have 8 or more characters");
+
+            if (string.IsNullOrEmpty(Username) || Username.Length < 3)
+                errors.Add("Username cannot be empty and must have 3 or more characters");
+
+            if (string.IsNullOrEmpty(Email))
+            errors.Add("Email cannot be empty.");
+            else
+            {
+                string regexPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$";
+                if (!Regex.IsMatch(Email, regexPattern))
+                    errors.Add("Invalid email format.");
+            }
+
+            if(string.IsNullOrEmpty(Password))
+                errors.Add("Password cannot be empty");
+            else
+            {
+                string regexPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$";
+                if(!Regex.IsMatch(Password, regexPattern))
+                    errors.Add("Invalid password format");
+            }
+        }
+
+        public List<string> GetErrors()
+        {
+            return errors;
+        }
     }
 }
