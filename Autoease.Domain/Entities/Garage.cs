a53466c2405ec3dcf583b4e2ef316e47101d1a94
Igor Oliveira <igoroliveira9827@gmail.com>
2024-07-17 22:04:52 -0300

    refactor: fix garage entity

--- Autoease.Domain/Entities/Garage.cs
+++ Autoease.Domain/Entities/Garage.cs
@@ -1,13 +1,19 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using Autoease.Domain.ValueObjects;
 
 namespace Autoease.Domain.Entities;
 
-public class GarageEntity
+public class GarageEntity : BaseEntity
 {
     protected GarageEntity() {}
-    public GarageEntity(string garageRegister, string garageName, Address address, string email, string phone, string businessHour, List<string> services)
+
+    public GarageEntity(
+        string garageRegister, 
+        string garageName, 
+        Address address, 
+        string email, 
+        string phone, 
+        string businessHour, 
+        List<string> services)
     {
         GarageRegister = garageRegister;
         GarageName = garageName;
@@ -18,7 +24,6 @@ public class GarageEntity
         Services = services;
     }
 
-    [Key]
     public string GarageRegister { get; private set; }
     public string GarageName { get; private set; }
     public Address Address { get; private set; }
