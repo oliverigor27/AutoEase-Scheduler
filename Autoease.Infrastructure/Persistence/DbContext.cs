c54489114e14b07d9a5973fadfc3a3a21c0fe801
Igor Oliveira <igoroliveira9827@gmail.com>
2024-07-17 22:05:58 -0300

    refactor: refactor db context

--- Autoease.Infrastructure/Persistence/DbContext.cs
+++ Autoease.Infrastructure/Persistence/DbContext.cs
@@ -1,27 +1,17 @@
-using Autoease.Domain.Aggregation;
 using Autoease.Domain.Entities;
-using Autoease.Domain.ValueObjects;
+using Autoease.Domain.Entities.Aggregate;
 using Microsoft.EntityFrameworkCore;
 
 namespace Autoease.Infrastructure.Persistence;
 
 public class DatabaseContext : DbContext
 {
-
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
     {
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Appointment>()
-            .OwnsOne(a => a.Veichle);
-
-        modelBuilder.Entity<GarageEntity>()
-            .OwnsOne(g => g.Address);
     }
 
     public DbSet<UserEntity> Users { get; set; }
     public DbSet<GarageEntity> Garages { get; set; }
     public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Veichle> Veichles { get; set; }
 }
