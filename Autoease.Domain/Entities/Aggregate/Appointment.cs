a6b2e38f5e3ce8ff29ae251025fe0737a33727e6
Igor Oliveira <igoroliveira9827@gmail.com>
2024-07-17 22:06:25 -0300

    feat: create a new appointment entity

---
+++ Autoease.Domain/Entities/Aggregate/Appointment.cs
@@ -0,0 +1,25 @@
+using Autoease.Domain.ValueObjects;
+
+namespace Autoease.Domain.Entities.Aggregate;
+
+public class Appointment : BaseEntity
+{
+    public Appointment(
+        DateTime dateAppoinment, 
+        Veichle veichle, 
+        ICollection<Services> 
+        services, 
+        GarageEntity garage
+    )
+    {
+        DateAppoinment = dateAppoinment;
+        Veichle = veichle;
+        Services = services;
+        Garage = garage;
+    }
+
+    public DateTime DateAppoinment { get; private set; }
+    public Veichle Veichle { get; private set; }
+    public ICollection<Services> Services { get; private set; }
+    public GarageEntity Garage { get; private set; }
+}
