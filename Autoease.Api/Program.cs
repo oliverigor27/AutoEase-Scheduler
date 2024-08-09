4f25f3d4bbc911a1fa2483c9665205d06c7028dd
Igor Oliveira <igoroliveira9827@gmail.com>
2024-08-08 23:52:37 -0300

    feat: add dabatase context service in Program archive

--- Autoease.Api/Program.cs
+++ Autoease.Api/Program.cs
@@ -1,23 +1,22 @@
-using Autoease.Data.MongoDB;
-using Autoease.Data.Settings;
+using Autoease.Data.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-builder.Services.Configure<AutoeaseDatabaseSettings>(
-    builder.Configuration.GetSection("AutoeaseDatabaseSettings")
+builder.Services.Configure<AppDbContext>(
+    builder.Configuration.GetSection("WebApiDatabase")
 );
 
-builder.Services.AddSingleton<UserDbService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase"))
+);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
