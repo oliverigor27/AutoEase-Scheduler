2702394b2d898c29ce8d3e262a1c8a00374c3a37
Igor Oliveira <igoroliveira9827@gmail.com>
2024-08-08 23:53:12 -0300

    refactor: fix database context

--- Autoease.Data/DatabaseContext/AppDbContext.cs
+++ Autoease.Data/DatabaseContext/AppDbContext.cs
@@ -1,8 +1,13 @@
+using Autoease.Domain.Entities;
+using Autoease.Domain.Entities.Aggregate;
 using Microsoft.EntityFrameworkCore;
 
 namespace Autoease.Data.DatabaseContext;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<GarageEntity> Garages { get; set; }
+    public DbSet<Appointment> Appointments { get; set; }
+    public DbSet<Veichle> Veichles { get; set; }
 }
