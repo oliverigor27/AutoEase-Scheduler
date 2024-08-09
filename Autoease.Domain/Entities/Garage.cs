2cb290780440d5fdde485ac250bf2f36795d8d76
Igor Oliveira <igoroliveira9827@gmail.com>
2024-08-09 00:00:09 -0300

    refactor: refactor garage entity

--- Autoease.Domain/Entities/Garage.cs
+++ Autoease.Domain/Entities/Garage.cs
@@ -4,31 +4,43 @@ namespace Autoease.Domain.Entities;
 
 public sealed class GarageEntity : BaseEntity
 {
-    protected GarageEntity() {}
+    private GarageEntity() {}
 
     public GarageEntity(
         string garageRegister, 
         string garageName, 
         Address address, 
-        string email, 
+        string email,
+        string password, 
         string phone, 
-        string businessHour, 
-        List<string> services)
+        DateTime openTime,
+        DateTime closeTime, 
+        List<Services> services)
     {
         GarageRegister = garageRegister;
         GarageName = garageName;
         Address = address;
         Email = email;
+        Password = password;
         Phone = phone;
-        BusinessHour = businessHour;
+        OpenTime = openTime;
+        CloseTime = closeTime;
         Services = services;
     }
 
-    public string GarageRegister { get; private set; }
-    public string GarageName { get; private set; }
-    public Address Address { get; private set; }
-    public string Email { get; private set; }
-    public string Phone { get; private set; }
-    public string BusinessHour { get; private set; }
-    public List<string> Services { get; private set; }
+    public required string GarageRegister { get; set; }
+    public required string GarageName { get; set; }
+    public required Address Address { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+    public required string Phone { get; set; }
+    public required DateTime OpenTime { get; set; }
+    public required DateTime CloseTime { get; set; }
+    public required List<Services> Services { get; set; }
+
+    public void AddServices(Services service)
+        => Services.Add(service);
+
+    public void RemoveService(Services service)
+        => Services.Remove(service);
 }
