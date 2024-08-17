5e882c98e92844844aa499bfd6aa43e4d672f29a
Igor Oliveira <igoroliveira9827@gmail.com>
2024-08-17 01:17:04 -0300

    fix: fix the wrong extensio for base entity

--- Autoease.Domain/Entities/BaseEntity.cs
+++ Autoease.Domain/Entities/BaseEntity.cs
@@ -1,12 +1,7 @@
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-
 namespace Autoease.Domain.Entities;
 
 public class BaseEntity
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
     public Guid Id { get; private set; }
     public DateTime Created_At { get; private set; }
 
