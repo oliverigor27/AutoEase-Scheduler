60c41b35ca1ba98a71878d9e54ecde0db483b8d1
Igor Oliveira <igoroliveira9827@gmail.com>
2024-08-20 23:12:21 -0300

    feat: add the model configurations in entity framework archive

--- Autoease.Data/DatabaseContext/AppDbContext.cs
+++ Autoease.Data/DatabaseContext/AppDbContext.cs
@@ -1,3 +1,4 @@
+using Autoease.Data.Configuration;
 using Autoease.Domain.Entities;
 using Autoease.Domain.Entities.Aggregate;
 using Microsoft.EntityFrameworkCore;
@@ -6,8 +7,23 @@ namespace Autoease.Data.DatabaseContext;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        new UserEntityConfiguration()
+            .Configure(modelBuilder.Entity<UserEntity>().ToTable("Users"));
+
+        new GarageEntityConfiguration()
+            .Configure(modelBuilder.Entity<GarageEntity>().ToTable("Garage"));
+
+        new ServicesEntityConfiguration()
+            .Configure(modelBuilder.Entity<Services>());
+    }
+
     public DbSet<UserEntity> Users { get; set; }
     public DbSet<GarageEntity> Garages { get; set; }
     public DbSet<Appointment> Appointments { get; set; }
     public DbSet<Veichle> Veichles { get; set; }
+    public DbSet<Services> Services { get; set; }
 }
